19d47cb68e1f0c2909143e2534328b0c39c93474
sakanaction317 <danki.karate9602@gmail.com>
2024-09-11 20:14:40 +0900

    kadai_mdnをアップロード

---
+++ kadai_mdn/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getUTCFullYear();
+const month = today.getMonth();
+const Day = today.getDate();
+
+console.log(year + '年' + (month + 1 ) + '月' + Day + '日');
